db3e609628b35c4169b62a338ac1278b7f2c94bf
kazui-git <coding.ghost999@gmail.com>
2024-07-21 16:56:57 +0800

    Quartz sync: Jul 21, 2024, 4:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "📒 Skrirrex-public-notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
